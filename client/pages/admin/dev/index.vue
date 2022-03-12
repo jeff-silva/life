e090539fafd4b23ecd98e6d585a75311a691a66c
Jeferson Inácio <jeferson.i.silva@gmail.com>
2022-03-11 21:24:56 -0300

    fixes

--- client/pages/admin/dev/index.vue
+++ client/pages/admin/dev/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="d-flex" v-if="$auth.user">
+        <div class="d-flex">
             <div class="bg-dark" style="height:100vh; width:200px;">
                 <ui-nav :items="navItems" text-color="#fff"></ui-nav>
             </div>
@@ -8,11 +8,6 @@
             <div class="flex-grow-1 p-3" style="height:100vh; overflow:auto;">
                 <nuxt-child></nuxt-child>
             </div>
-        </div>
-
-        <div class="mx-auto mt-5" style="max-width:400px;" v-else>
-            <div class="mb-3">É necessário estar logado para visualizar este conteúdo.</div>
-            <ui-auth-login :redirect="$route.path"></ui-auth-login>
         </div>
     </div>
 </template>
@@ -22,10 +17,10 @@ export default {
     data() {
         return {
             navItems: [
-                {label:"Home", to:"/dev/", children:[]},
-                {label:"Endpoints", to:"/dev/endpoints/", children:[]},
+                {label:"Home", to:"/admin/dev/", children:[]},
+                {label:"Endpoints", to:"/admin/dev/endpoints/", children:[]},
                 {label:"Components", children:[
-                    {label:"Dropdown", to:"/dev/components-dropdown/", children:[]},
+                    {label:"Dropdown", to:"/admin/dev/components-dropdown/", children:[]},
                 ]},
             ],
         };
--- client/pages/admin/dev/index/index.vue
+++ client/pages/admin/dev/index/index.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        Página inicial
+        <h3>Instalação</h3>
+        <pre class="bg-dark p-2 m-0 text-white">composer install<br>npm install<br>php artisan app:install</pre>
+        <br>
+        
+        <h3>Criar uma tabela</h3>
+        <pre class="bg-dark p-2 m-0 text-white">php artisan app:table-generate</pre>
+        <p>Após executar o comando, informe o nome da tabela para o banco.</p>
+        <br>
     </div>
 </template>
